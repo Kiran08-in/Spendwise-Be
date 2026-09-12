de0be6e3b6750463513020289485032a8c98a243
Kiran08-in <bisoyikiran0@gmail.com>
2026-09-12 12:36:54 +0530

    add get_db dependency and GET /transactions endpoint, tested via /docs

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+import models
+from database import engine
+from database import SessionLocal
+
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "api is running"}
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+@app.get("/transactions")
+def get_transactions(db: Session = Depends(get_db)):
+    return db.query(models.Transaction).all()
