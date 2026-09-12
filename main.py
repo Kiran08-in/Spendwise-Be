bb0f6e0ea6be935e038a74817c7251175e0b6e3d
Kiran08-in <bisoyikiran0@gmail.com>
2026-09-12 13:22:40 +0530

    add post and de;ete endpoints

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 import models
+import schemas
 from database import engine
 from database import SessionLocal
 
@@ -17,6 +18,24 @@ def get_db():
         yield db
     finally:
         db.close()
+
 @app.get("/transactions")
 def get_transactions(db: Session = Depends(get_db)):
     return db.query(models.Transaction).all()
+
+@app.post("/transactions")
+def create_transactions(transaction: schemas.TransactionCreate, db: Session = Depends(get_db)):
+    new_transaction = models.Transaction(**transaction.model_dump())
+    db.add(new_transaction)
+    db.commit()
+    db.refresh(new_transaction)
+    return new_transaction
+
+@app.delete('/transactions/{transaction_id}')
+def delete_transaction(transaction_id: int, db: Session = Depends(get_db)):
+    transaction = db.query(models.Transaction).filter(models.Transaction.id == transaction_id).first()
+    if not transaction:
+        raise HTTPException(status_code = 404, detail = "Transaction not found")
+    db.delete(transaction)
+    db.commit()
+    return {"message": "Transaction deleted successfully"}
