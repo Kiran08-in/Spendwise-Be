4321118255247db858c7e5794a2303b5b0080cca
Kiran08-in <bisoyikiran0@gmail.com>
2026-09-13 14:26:13 +0530

    add CORS middleware

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ import models
 import schemas
 from database import engine
 from database import SessionLocal
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
